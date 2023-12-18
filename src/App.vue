c34249c67067320d160b17c43e18127dd8f189bf
aimeliala <idstack@outlook.co.id>
2023-12-18 21:25:13 +0700

    use json data to display quiz

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from "vue";
+import srcQuiz from "./data/quizes.json";
+
+const quizes = ref(srcQuiz);
+</script>
+
 <template>
   <main class="container">
     <header>
@@ -5,14 +12,11 @@
       <input id="search-input" type="text" name="" placeholder="Search..." />
     </header>
     <section id="quiz-container">
-      <div class="card">
-        <img
-          src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="programming"
-        />
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" alt="programming" />
         <div class="card-text">
-          <h2>Programming</h2>
-          <p>5 questions</p>
+          <h2>{{ quiz.title }}</h2>
+          <p>{{ quiz.questions.length }} Questions</p>
         </div>
       </div>
     </section>
