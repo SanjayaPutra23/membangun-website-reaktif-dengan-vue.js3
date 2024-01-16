a909067c0f89b30c50fbe4eed0a4896a407eb252
aim <ahmadhakim404@gmail.com>
2024-01-16 20:41:33 +0700

    use computed to create value from reactive resource

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -2,7 +2,7 @@
 import Question from '@/components/Question.vue';
 import QuestionHeader from '@/components/QuestionHeader.vue';
 import { useRoute } from 'vue-router';
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import quizes from '../data/quizes.json';
 
 const route = useRoute();
@@ -10,23 +10,27 @@ const quizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === quizId);
 
 const currentQuestionIndex = ref(0);
-const questionStatus = ref(
-	`${currentQuestionIndex.value + 1}/${quiz.questions.length}`
-);
+// const questionStatus = ref(
+// 	`${currentQuestionIndex.value + 1}/${quiz.questions.length}`
+// );
 
-watch(
-	() => currentQuestionIndex.value,
-	() => {
-		questionStatus.value = `${currentQuestionIndex.value + 1}/${
-			quiz.questions.length
-		}`;
-	}
-);
+// watch(
+// 	() => currentQuestionIndex.value,
+// 	() => {
+// 		questionStatus.value = `${currentQuestionIndex.value + 1}/${
+// 			quiz.questions.length
+// 		}`;
+// 	}
+// );
 
-watch(currentQuestionIndex, () => {
-	questionStatus.value = `${currentQuestionIndex.value + 1}/${
-		quiz.questions.length
-	}`;
+// watch(currentQuestionIndex, () => {
+// 	questionStatus.value = `${currentQuestionIndex.value + 1}/${
+// 		quiz.questions.length
+// 	}`;
+// });
+
+const questionStatus = computed(() => {
+	return `${currentQuestionIndex.value + 1}/${quiz.questions.length}`;
 });
 </script>
 <template>
