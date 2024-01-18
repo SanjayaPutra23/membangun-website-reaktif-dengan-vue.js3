e17931481bceda9fb246b90bf5ffd3448210c841
aim <ahmadhakim404@gmail.com>
2024-01-19 01:51:08 +0700

    make dynamic page number of question

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,7 +1,7 @@
 <script setup>
 import QuizHeader from '@/components/QuizHeader.vue';
 import QuizContent from '@/components/QuizContent.vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import quizes from '../data/quizes.json';
 
@@ -10,11 +10,29 @@ const quizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === quizId);
 
 const currentQuestionIndex = ref(0);
+const questionPage = ref(
+	`${currentQuestionIndex.value + 1} / ${quiz.questions.length}`
+);
+
+watch(
+	() => currentQuestionIndex.value,
+	() => {
+		questionPage.value = `
+			${currentQuestionIndex.value + 1} / ${quiz.questions.length}
+		`;
+	}
+);
 </script>
 
 <template>
-	<QuizHeader />
+	<QuizHeader :questionPage="questionPage" />
 	<QuizContent :question="quiz.questions[currentQuestionIndex]" />
+	<button
+		@click="currentQuestionIndex++"
+		:disabled="currentQuestionIndex === quiz.questions.length - 1"
+	>
+		Next
+	</button>
 </template>
 
 <style scoped></style>
