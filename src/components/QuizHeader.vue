89af3a26be6831eb07c493c699138c87479d5e9e
aim <ahmadhakim404@gmail.com>
2024-01-21 13:01:26 +0700

    make progress bar function

--- src/components/QuizHeader.vue
+++ src/components/QuizHeader.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { defineProps } from 'vue';
-const { questionPage } = defineProps(['questionPage']);
+const { questionPage, barPercentage } = defineProps([
+	'questionPage',
+	'barPercentage',
+]);
 </script>
 <template>
 	<header>
 		<h4>Questions {{ questionPage }}</h4>
 		<div class="bar">
-			<div class="bar-progress"></div>
+			<div class="bar-progress" :style="{ width: barPercentage }"></div>
 		</div>
 	</header>
 </template>
@@ -25,7 +28,7 @@ header h4 {
 }
 .bar-progress {
 	height: 100%;
-	width: 50%;
+	width: 0%;
 	background-color: #adf7b6;
 }
 </style>
